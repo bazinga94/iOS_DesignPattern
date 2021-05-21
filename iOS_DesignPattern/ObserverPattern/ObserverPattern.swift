5bd5689860cd515b8a3912dbdc04c6d9379bd553
이종호 <jongho@mail.shinhan.dev>
2021-05-21 13:04:48 +0900

    Observer 생성 및 관찰자 구현

--- iOS_DesignPattern/ObserverPattern/ObserverPattern.swift
+++ iOS_DesignPattern/ObserverPattern/ObserverPattern.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol Observer {
+	var id: Int { get }
+	func update(_ notifyValue: Int)
+}
+
+class Subject {
+	private var observerArray: [Observer] = [Observer]()
+	private var _number = Int()
+
+	/// 값이 변경되면 notify() 호출
+	var number: Int {
+		set {
+			_number = newValue
+			notify()
+		}
+
+		get {
+			return _number
+		}
+	}
+
+	/// Observer 등록
+	/// - Parameter observer: Observer
+	func attachObserver(observer: Observer) {
+		observerArray.append(observer)
+	}
+
+	/// Observer 알림
+	func notify() {
+		for observer in observerArray {
+			observer.update(number)
+		}
+	}
+}
+
