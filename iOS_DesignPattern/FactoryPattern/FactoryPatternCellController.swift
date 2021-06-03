0ab30e0f5333f3b23321d04f8bac63d8b6df4b0f
Jongho Lee <bazinga940@gmail.com>
2021-06-03 22:53:47 +0900

    protocol 선언

--- iOS_DesignPattern/FactoryPattern/FactoryPatternCellController.swift
+++ iOS_DesignPattern/FactoryPattern/FactoryPatternCellController.swift
@@ -5,4 +5,44 @@
 //  Created by Jongho Lee on 2021/06/02.
 //
 
-import Foundation
+import UIKit
+
+protocol CellectionViewSectionController {
+
+}
+
+protocol CollectionViewCellController {
+	static func registerCell(on collectionView: UICollectionView)
+	func cellFromCollectionView(_ collectionView: UICollectionView, itemAt indexPath: IndexPath) -> UICollectionViewCell
+	func didSelectCell(itemAt indexPath: IndexPath)
+}
+
+class CollectionViewCellControllerFactory {
+	func registerCells(on collectionView: UICollectionView) {
+	}
+
+	func cellController(with item: ItemModelProtocol) -> [CollectionViewCellController] {
+	}
+}
+
+protocol ItemModelProtocol {
+}
+
+class YGTransferMemoCollectionViewCellController: CollectionViewCellController {
+
+	private let item: ItemModelProtocol
+
+	init(item: ItemModelProtocol) {
+		self.item = item
+	}
+
+	static func registerCell(on collectionView: UICollectionView) {
+	}
+
+	func cellFromCollectionView(_ collectionView: UICollectionView, itemAt indexPath: IndexPath) -> UICollectionViewCell {
+	return UICollectionViewCell()
+	}
+
+	func didSelectCell(itemAt indexPath: IndexPath) {
+	}
+}
