e90b6449b047b12576ef959d91780b66e2864849
이종호 <bazinga940@gmail.com>
2021-08-18 17:07:35 +0900

    CellFactory 그룹 추가

---
+++ xcode project/iOS_DesignPattern/FactoryPattern/CellFactory/FactoryPatternCollectionCellController.swift
@@ -0,0 +1,57 @@
+//
+//  FactoryPatternCollectionCellController.swift
+//  iOS_DesignPattern
+//
+//  Created by Jongho Lee on 2021/06/02.
+//
+
+import UIKit
+
+protocol CollectionCellController {
+	static func registerCell(on collectionView: UICollectionView)
+	func cellFromCollectionView(_ collectionView: UICollectionView, itemAt indexPath: IndexPath) -> UICollectionViewCell
+	func didSelectCell(itemAt indexPath: IndexPath)
+}
+
+protocol CollectionCellControllerFactory {
+	func registerCells(on collectionView: UICollectionView)
+}
+
+class SampleCollectionViewCellControllerFactory: CollectionCellControllerFactory {
+	func registerCells(on collectionView: UICollectionView) {
+		SampleCollectionViewCellController.registerCell(on: collectionView)
+	}
+
+	func cellController(with item: ItemModelProtocol) -> [CollectionCellController] {	// section이 있는 경우 [[]] return
+		return [SampleCollectionViewCellController(item: item)]
+	}
+}
+
+protocol ItemModelProtocol {
+}
+
+class SampleCollectionViewCellController: CollectionCellController {
+
+	private let item: ItemModelProtocol
+
+	init(item: ItemModelProtocol) {
+		self.item = item
+	}
+
+	static func registerCell(on collectionView: UICollectionView) {
+	}
+
+	func cellFromCollectionView(_ collectionView: UICollectionView, itemAt indexPath: IndexPath) -> UICollectionViewCell {
+	return UICollectionViewCell()
+	}
+
+	func didSelectCell(itemAt indexPath: IndexPath) {
+	}
+}
+
+// 팩토리 메서드 패턴으로 Collection View Cell 관리
+// 새로운 cell을 추가하려고 할 때 view controller의 수정이 필요 없다.
+// 하지만 새로운 collection view에 대한 factory pattern을 적용할 때 재사용 되는 코드들이 많음...
+// collection view마다 데이터 모델이 다르기 때문에 cell controller factory가 수정이 많이 될것 같음
+// https://tech.busuu.com/dealing-with-different-kinds-of-cells-in-swift-part-1-of-3-18c6cd10a0b3
+// 해당 url의 방식을 참고해서 공부해보자!
---
+++ xcode project/iOS_DesignPattern/FactoryPattern/CellFactory/FactoryPatternForCellType.swift
@@ -0,0 +1,176 @@
+//
+//  FactoryPatternForCellType.swift
+//  iOS_DesignPattern
+//
+//  Created by Jongho Lee on 2021/06/08.
+//
+
+import UIKit
+
+// https://tech.busuu.com/dealing-with-different-kinds-of-cells-in-swift-part-2-of-3-3fe73b0c50c6 참고
+// 기본적으로 collection view와 table view는 동일한 cell 처리 구조를 가지고 있음
+// associated type과 protocol을 활용하여 더 간결한 controller를 만들어 보자
+
+protocol ReusableCell: class {
+	associatedtype CellHolder: ReusableCellHolder
+}
+
+extension UITableViewCell: ReusableCell {
+	typealias CellHolder = UITableView
+}
+
+//extension UITableViewCell: ReusableCell {
+//	typealias CellHolder = UICollectionView
+//} -> compile error 발생
+
+extension UICollectionViewCell: ReusableCell {
+	typealias CellHolder = UICollectionView
+}
+
+protocol ReusableCellHolder: class {
+	associatedtype CellType: ReusableCell where CellType.CellHolder == Self
+
+	func register(_ nib: UINib?, forCellWithReuseIdentifier identifier: String)
+	func dequeueReusableCell(withReuseIdentifier identifier: String, for indexPath: IndexPath) -> CellType
+}
+
+extension UITableView: ReusableCellHolder {
+
+	func register(_ nib: UINib?, forCellWithReuseIdentifier identifier: String) {
+		register(nib, forCellReuseIdentifier: identifier)
+	}
+
+	func dequeueReusableCell(withReuseIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+		return dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+	}
+}
+// CellType은 UITableViewCell로 지정
+
+extension UICollectionView: ReusableCellHolder {
+
+}
+// UICollectionView에는 이미 해당 protocol이 구현되어 있다
+
+protocol CellControllerType {
+	associatedtype ViewType: ReusableCellHolder
+
+	static func registerCell(on reusableCellHolder: ViewType)
+	func cellFromReusableCellHolder(_ reusableCellHolder: ViewType, forIndexPath indexPath: IndexPath) -> ViewType.CellType
+	func didSelectCell(itemAt indexPath: IndexPath)
+}
+
+class CellController<T: ReusableCellHolder>: CellControllerType {
+
+	class var cellClass: AnyClass {		// override 가능
+		fatalError("Must be implemented by children")
+	}
+
+	static var cellIdentifier: String {
+		return String(describing: cellClass)
+	}
+
+	static func registerCell(on reusableCellHolder: T) {
+		let bundle = Bundle(for: cellClass)
+		let nib = UINib(nibName: cellIdentifier, bundle: bundle)
+		reusableCellHolder.register(nib, forCellWithReuseIdentifier: cellIdentifier)
+	}
+
+	func cellFromReusableCellHolder(_ reusableCellHolder: T, forIndexPath indexPath: IndexPath) -> T.CellType {
+		let cell = reusableCellHolder.dequeueReusableCell(withReuseIdentifier: type(of: self).cellIdentifier, for: indexPath)
+		configureCell(cell)
+		return cell
+	}
+
+	func configureCell(_ cell: T.CellType) {
+		// 자식 클래스에서 override하여 구현
+	}
+
+	func didSelectCell(itemAt indexPath: IndexPath) {
+		// 자식 클래스에서 override하여 구현
+	}
+}
+
+protocol Item {
+}
+
+class SampleTableCellController: CellController<UITableView> {
+	private let item: Item
+
+	init(item: Item) {
+		self.item = item
+	}
+
+	override class var cellClass: AnyClass {
+		return SampleTableCellController.self
+	}
+
+	override func configureCell(_ cell: UITableViewCell) {
+//		let sampleCell = cell as! SampleCell
+//		... cell configure 로직
+	}
+
+	override func didSelectCell(itemAt indexPath: IndexPath) {
+//		... cell 선택 로직
+	}
+}
+
+// -> Bundle, Nib 에 대해 신경쓰거나 register를 따로 해줄 필요가 없음
+// dequeue cell을 해줄 필요 없이 configure cell 만 해주면 됨
+
+protocol ItemList {
+	var item: Item { get }
+}
+
+class SampleTableCellControllerFactory {
+	func registerCells(on tableView: UITableView) {
+		SampleTableCellController.registerCell(on: tableView)
+	}
+
+	func cellController(with items: ItemList) -> [CellController<UITableView>] {
+		return [SampleTableCellController(item: items.item)]
+	}
+}
+
+// -> 여전히 불편한 점은 있음!, SampleTableCellController에서 cellClass를 지정해주는 부분이나 configureCell에서 Cell을 타입 캐스팅 해줘야 한다.
+// ReusableCell을 선언
+
+//protocol ReusableCell: class {
+//	associatedtype CellHolder: ReusableCellHolder
+//}
+
+// 더욱 더 Generic 한 Cell Controller 생성
+
+class GenericCellController<T: ReusableCell>: CellController<T.CellHolder> {
+
+	typealias BaseReusableCell = T.CellHolder.CellType
+
+	final override class var cellClass: AnyClass {
+		return T.self
+	}
+
+	final override func configureCell(_ cell: BaseReusableCell) {
+		let cell = cell as! T
+		configureCell(cell)
+	}
+
+	func configureCell(_ cell: T) {
+
+	}
+}
+
+class SampleTableViewCell: UITableViewCell {
+}
+
+class SampleTableGenericCellController: GenericCellController<SampleTableViewCell> {
+	private let item: Item
+
+	init(item: Item) {
+		self.item = item
+	}
+
+	override func configureCell(_ cell: SampleTableViewCell) {
+	}
+
+	override func didSelectCell(itemAt indexPath: IndexPath) {
+	}
+}
