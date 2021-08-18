3bf53b529ecfe5d66c564174aedbb97b46df5457
이종호 <bazinga940@gmail.com>
2021-08-18 17:24:20 +0900

    Sample viewcontroller 생성

--- xcode project/iOS_DesignPattern/FactoryPattern/CellFactory/Sample/CellFactoryViewController.swift
+++ xcode project/iOS_DesignPattern/FactoryPattern/CellFactory/Sample/CellFactoryViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class CellFactoryViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
 	}
 }
